ee263cba16cedafefb12ed37a5783c967a9d3541
SahidJamal-1 <75063419+Sahid-321@users.noreply.github.com>
2023-03-30 20:27:38 +0530

    changes

--- dist/swagger.js
+++ dist/swagger.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const swaggerDefinition = {
     openapi: '3.0.0',
     info: {
-        title: 'Annamandir API',
+        title: 'Dish Management API',
         version: '1.0.0',
-        description: 'This is annamandir API ',
+        description: 'This is Dish Management API ',
     },
     servers: [
         {
